218cc9290b62234e9f6c2af2dd64de22f896537b
Jakub Tustanowski <jakub.tustanowski@gmai.com>
2017-03-30 12:20:26 +0200

    Moving files

--- judge-backend/src/main/java/judge/Entity/Submission.java
+++ judge-backend/src/main/java/judge/Entity/Submission.java
@@ -1,50 +1,86 @@
 package judge.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-public class Submission {
+public class Submission
+{
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
+    private Student author;
     private String code;
     private Integer compilationCode;
     private Integer runCode;
 
-    public Submission(String code, int compilationCode, int runCode) {
+    public Submission(Student author, String code, Integer compilationCode, Integer runCode)
+    {
+        this.author = author;
         this.code = code;
         this.compilationCode = compilationCode;
         this.runCode = runCode;
     }
 
-    public Submission() {
+    public Submission(String code, Integer compilationCode, Integer runCode)
+    {
+        this.code = code;
+        this.compilationCode = compilationCode;
+        this.runCode = runCode;
     }
 
-    public String getCode() {
+    public Submission()
+    {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId()
+    {
+        return id;
+    }
+
+    @ManyToOne
+    public Student getAuthor()
+    {
+        return author;
+    }
+
+    public String getCode()
+    {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public int getCompilationCode() {
+    public Integer getCompilationCode()
+    {
         return compilationCode;
     }
 
-    public void setCompilationCode(int compilationCode) {
-        this.compilationCode = compilationCode;
-    }
-
-    public int getRunCode() {
+    public Integer getRunCode()
+    {
         return runCode;
     }
 
-    public void setRunCode(int runCode) {
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public void setAuthor(Student author)
+    {
+        this.author = author;
+    }
+
+    public void setCode(String code)
+    {
+        this.code = code;
+    }
+
+    public void setCompilationCode(Integer compilationCode)
+    {
+        this.compilationCode = compilationCode;
+    }
+
+    public void setRunCode(Integer runCode)
+    {
         this.runCode = runCode;
     }
 }
