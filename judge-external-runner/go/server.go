8f4a48cadf4f202751d8826ea263cc458b47e8b5
gborkowski <gb.grzegorzborkowski@gmail.com>
2017-05-03 12:51:50 +0200

    Add BROKEN using docker container in external runner

--- judge-external-runner/go/server.go
+++ judge-external-runner/go/server.go
@@ -3,7 +3,6 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
     "io"
     "os"
     "os/exec"
@@ -13,10 +12,9 @@ import (
 
 func upload(w http.ResponseWriter, r *http.Request) {
        log.Println("method:", r.Method)
-       if r.Method == "GET" {
-           w.Write([]byte("GO server is active. Use POST to submit your solution."))
-       } else {
+       if r.Method == "POST" {
            log.Println("Processing new SUBMISSION.")
+           // https://github.com/astaxie/build-web-application-with-golang/blob/master/de/04.5.md
            r.ParseMultipartForm(32 << 20)
            file, handler, err := r.FormFile("file")
            if err != nil {
@@ -32,58 +30,57 @@ func upload(w http.ResponseWriter, r *http.Request) {
            defer f.Close()
            io.Copy(f, file)
 
-           compilationCode, runCode := process(w, r, "examine/" + handler.Filename)
+           compilationCode, runCode := process("examine/" + handler.Filename)
 
            w.Header().Set("compilationCode", compilationCode)
            w.Header().Set("runCode", runCode)
+       } else {
+           w.Write([]byte("GO server is active. Use POST to submit your solution."))
        }
 }
 
 // @filename contains "examine" directory
-func process(w http.ResponseWriter, r *http.Request, filename string) (string, string) {
-    compilationCode := "-1"
-    runCode := "-1"
-
-    execFilename := filename  + ".out"
-    cmdCompile := exec.Command("gcc", "-o" + execFilename, filename)
-    errCompile := cmdCompile.Run()
-
-    if errCompile != nil {
-        log.Println("COMPILATION FAILED")
-        log.Println(errCompile)
-    } else {
-        compilationCode = "0"
-        cmdRun := exec.Command("./" + execFilename)
+// there is assumption that docker is installed where server.go is running
+// and the container is already pulled
+// TODO: handle situation when container is not pulled
+// TODO: somehow capture if compilation wasn't successful and
+// TODO: distinguish it from possible execution / time limit / memory limit error
+func process(filename string) (string, string) {
+    cmdRun := exec.Command(getDockerExecutinoCommanand(filename))
         var out bytes.Buffer
         cmdRun.Stdout = &out
 
         if errRun := cmdRun.Start(); errRun != nil {
             log.Fatal(errRun)
         }
-        timer := time.AfterFunc(10 * time.Second, func() {
-            cmdRun.Process.Kill()
-        })
-        errTimeout := cmdRun.Wait()
-        timer.Stop()
-        if errTimeout == nil {
-            log.Println(execFilename + " - SUCCEED")
-            log.Printf(out.String())
-            runCode = "0"
-        } else {
-            log.Println(execFilename + " - TIMEOUT")
-            runCode = "2"
-        }
-    }
-    cmdClean := exec.Command("rm", filename, execFilename)
-    errClean := cmdClean.Start()
-    if errClean != nil {
-        log.Println(errClean)
-    }
-    return compilationCode, runCode
+
+    //cmdClean := exec.Command("rm", filename, execFilename)
+    //errClean := cmdClean.Start()
+    //if errClean != nil {
+    //    log.Println(errClean)
+    //}
+    return "2", "8"
 }
 
 // Creates examine directory if it doesn't exist.
 // If examine directory already exists, then comes an error.
+
+func getDockerExecutinoCommanand(filename string) string{
+    dir, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    // -----------------------------------------------
+    var prefix = "docker run -e "
+    var f00 = "F00='" + filename + "' "
+    var working_directory = "-v " + dir + "/" + filename
+    var working_folder = ":/WORKING_FOLDER/" + filename + " "
+    var container_name = "tusty53/c_runner"
+    result := prefix + f00 + working_directory + working_folder + container_name
+    return result
+}
+
 func prepareDir() {
     cmdMkdir := exec.Command("mkdir", "examine")
     errMkdir := cmdMkdir.Run()
