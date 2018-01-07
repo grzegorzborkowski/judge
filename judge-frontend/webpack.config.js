0a01b28f7948f01743286d9aac40d89c97c646db
jakubtustanowski <tusty53@gmail.com>
2018-01-07 10:11:38 +0100

    File-loader used to load the images.

--- judge-frontend/webpack.config.js
+++ judge-frontend/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       {
           test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=25000'
+          loader: 'file-loader?limit=25000'
       },
     ]
   },
