6c38bc307a29713cf0b82b4168f486706586dbce
jakubtustanowski <tusty53@gmail.com>
2018-01-07 10:19:29 +0100

    Changed buttons' descriptions to clarify their purpose

--- judge-frontend/src/js/modules/Submissions.js
+++ judge-frontend/src/js/modules/Submissions.js
@@ -45,7 +45,7 @@ class Submissions extends React.Component {
                     <th>Tests passed</th>
                     <th>Runtime [s]</th>
                     <th>Code</th>
-                    <th>Edit</th>
+                    <th>Edition</th>
                     <th>Date</th>
                     <th>Result</th>
                 </tr>
@@ -60,8 +60,8 @@ class Submissions extends React.Component {
                         <td>{constants.getRunStatus(submission.runCode)}</td>
                         <td>{submission.testsPositive}/{submission.testsTotal}</td>
                         <td>{submission.timeTaken} </td>
-                        <td><a onClick={() => { alert(submission.code) }}>Click</a></td>
-                        <td><Link to={`/problem/${submission.problem.id}/${submission.id}`}>Click</Link></td>
+                        <td><a onClick={() => { alert(submission.code) }}>View</a></td>
+                        <td><Link to={`/problem/${submission.problem.id}/${submission.id}`}>Change</Link></td>
                         <td>{new Date(submission.date).toLocaleDateString(window.navigator.userLanguage || window.navigator.language, options)} </td>
                         <td>{constants.getResultIcon(submission.runCode,
                           submission.testsPositive, submission.testsTotal)}</td>
